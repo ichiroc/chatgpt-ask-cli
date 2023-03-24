c9debfcbcf03881880fc53469da152edd5dd0368
Morikiyo INOHA <morikiyo@sonicgarden.jp>
2023-03-24 10:52:54 +0900

    update: 5回メッセージのやり取りができるように

--- chat.rb
+++ chat.rb
@@ -1,11 +1,22 @@
 require 'openai'
 
+def chat_message(client)
+  puts "\nYou >"
+  message = gets.strip
+
+  puts "\nAI >"
+
+  response = client.chat(
+    parameters: {
+        model: "gpt-3.5-turbo", # Required.
+        messages: [{ role: "user", content: message }], # Required.
+        temperature: 0.7,
+    })
+
+  puts response.dig("choices", 0, "message", "content")
+end
+
 def main
-  puts "質問を入力してください >"
-  question = gets.strip
-
-  puts "\n... ...\n\n"
-
   OpenAI.configure do |config|
     config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
     config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
@@ -14,14 +25,9 @@ def main
 
   client = OpenAI::Client.new
 
-  response = client.chat(
-    parameters: {
-        model: "gpt-3.5-turbo", # Required.
-        messages: [{ role: "user", content: question }], # Required.
-        temperature: 0.7,
-    })
-
-  puts response.dig("choices", 0, "message", "content")
+  5.times do |n|
+    chat_message(client)
+  end
 end
 
 main
